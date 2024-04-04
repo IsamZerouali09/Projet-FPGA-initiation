6de4b80d31f79c2eb3ccd14a4f6deb25b3267893
Isam Zerouali <isam.zerouali07@gmail.com>
2024-04-04 14:54:20 -0400

    APIO-P4: Create led counter with oscillator 12 MHz

--- apio/button_counter/button_counter.v
+++ apio/button_counter/button_counter.v
@@ -3,23 +3,37 @@
 module button_counter (
 
 	// Inputs
-	input		[1:0]	pmod,
+	input				pmod,
+	input				clk,
 	
 	// Outputs
 	output reg	[3:0]	led
 );
 
 	wire rst;
-	wire clk;
+	reg clk_1Hz;
+	reg [31:0] inc;
+	//localparam [31:0] max_count = 6000000;
 	
 	// Reset is the inverse of the first button
 	assign rst = ~pmod[0];
 	
-	// Clock signal is the inverse of second button
-	assign clk = ~pmod[1];
+	// Clock signal of 1 Hz with oscillator of 12 MHz
+	always @ (posedge clk or posedge rst) begin
+		if (rst == 1'b1) begin
+			//led		<=	4'b0;
+			clk_1Hz	<=	1'b0;
+			inc		<=	32'b0;
+		end else if (inc <= 6000000) begin
+			inc 	<= inc + 1;
+		end else begin
+			inc <= 0;
+			clk_1Hz <= ~clk_1Hz;
+		end
+	end
 
 	// Count up on clock rising edge or reset on button push
-	always @ (posedge clk or posedge rst) begin
+	always @ (posedge clk_1Hz or posedge rst) begin
 		if (rst == 1'b1) begin
 			led <= 4'b0;
 		end else begin
